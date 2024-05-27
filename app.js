0dc625504b94c1bcb0226d21788a08ddb52e029d
junwon <wlals5459@naver.com>
2024-05-27 13:25:55 +0900

    branch commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const http=require("http");
+const express=require("express");
+const app=express();
+
+app.get("/", (req, res)=>{
+    res.end("<h1>Hello world!</h1>");
+    
+})
+app.get("/home", (req, res)=>{
+    res.end("<h1>Hello world!@</h1>");
+    
+})
+const server=http.createServer(app);
+server.listen(3000, ()=>{
+    console.log("Run on server http://localhost:3000");
+
+})
